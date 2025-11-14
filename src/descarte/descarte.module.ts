49eeb6b1fab921054c6fa9ded4636a84e7810bef
viniciushenrique67-oss <vinicius.henrique67@gmail.com>
2025-11-13 22:12:23 -0300

    Primeiro commit do projeto NestJS

--- src/descarte/descarte.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DescarteController } from './descarte.controller';
-import { DescarteService } from './descarte.service';
-
-@Module({
-  controllers: [DescarteController],
-  providers: [DescarteService]
-})
-export class DescarteModule {}
